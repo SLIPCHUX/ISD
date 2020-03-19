5852844446177e8e6b37e06f4a5953c3527bff09
Denys Slipchuk <54553568+SLIPCHUX@users.noreply.github.com>
2020-03-19 12:58:58 +0200

    Add files via upload

---
+++ InUpperCase.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+const int lower = 32;
+
+const char* ToUpperCase(char str[], size_t n)
+{
+	char* res = str;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		if ('A' <= str[i] && str[i] <= 'z')
+		{
+			if (str[i] > 'Z')
+			{
+				str[i] -= lower;
+			}
+
+		}
+	}
+	return res;
+}
+
+int main()
+{
+	char str[] = "This is C++";
+	const size_t n = sizeof(str) / sizeof(str[0]);
+
+	std::cout << ToUpperCase(str, n);
+
+	system("Pause");
+	return 0;
+}
+/*
+DENYS SLIPCHUK
+For a given string, display the same string in upper case.
+Example:
+Original string: "This is C++"
+Expected Result: "THIS IS C ++"
+*/
